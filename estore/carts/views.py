b58eb43c6cdd74f863565817c84ec9e0a6d8e3c6
shangugregory <shangugregory@gmail.com>
2024-02-21 18:56:45 +0300

    My second Commit 21st dec 2024

--- estore/carts/views.py
+++ estore/carts/views.py
@@ -33,36 +33,65 @@ def add_cart(request, product_id):
             cart_id = _cart_id(request)
         )
     cart.save()
+    
+    is_cart_item_exist = CartItem.objects.filter(product = product, cart= cart).exists()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart = cart)
-        cart_item.quantity += 1
-        cart_item.save()
-    except ObjectDoesNotExist:
+    if is_cart_item_exist:
+        cart_item = CartItem.objects.filter(product=product, cart = cart)
+
+        ex_var_list =[]
+        id=[]
+        for item in cart_item:
+            existing_variation = item.variation.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+        print(ex_var_list)
+
+        if product_variation in ex_var_list:
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product = product, id = item_id)
+            item.quantity +=1
+            item.save()
+        else:
+            item = CartItem.objects.create(product = product, quantity  = 1, cart = cart)
+            if len(product_variation)>0:
+                item.variation.clear()
+                item.variation.add(*product_variation)
+        #cart_item.quantity += 1
+            item.save()
+    else:
         cart_item = CartItem.objects.create(
             product=product,
             quantity = 1,
             cart = cart,
         )
+        if len(product_variation)>0:
+            cart_item.variation.clear()
+            cart_item.variation.add(*product_variation)
         cart_item.save()
     return redirect('cart')
 
-def remove_cart(request, product_id):
+def remove_cart(request, product_id, cart_item_id):
     cart = MyCart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product, id= product_id)
-    cart_item = CartItem.objects.get(product = product, cart = cart)
-    if cart_item.quantity >1:
-        cart_item.quantity -= 1
-        cart_item.save()
 
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product = product, cart = cart, id = cart_item_id)
+        if cart_item.quantity >1:
+            cart_item.quantity -= 1
+            cart_item.save()
+
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect('cart')
 
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id, cart_item_id):
     cart = MyCart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product, id = product_id)
-    cart_item = CartItem.objects.get(product = product, cart = cart)
+    cart_item = CartItem.objects.get(product = product, cart = cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
 def Cart(request, total = 0, quantity = 0, cart_items = None):
--- estore/store/views.py
+++ estore/store/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
@@ -46,4 +47,13 @@ def Product_Detail(request, category_slug ,product_slug):
     return render(request, 'home/product-detail.html', context)
 
 def Search(request):
-    return render(request, 'store/store.html')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains = keyword))
+            product_count = products.count()
+        context={
+            'products':products,
+            'product_count':product_count
+        }
+    return render(request, 'store/store.html', context)
